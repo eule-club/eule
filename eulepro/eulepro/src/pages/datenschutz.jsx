259401461517bf5e45be49e49e25d7b23e1620e8
eule-club <eulepro7@gmail.com>
2025-05-16 13:57:25 +0200

    Create datenschutz.jsx

---
+++ eulepro/eulepro/src/pages/datenschutz.jsx
@@ -0,0 +1,8 @@
+const datenschutz = () => (
+  <div className="p-10">
+    <h1 className="text-2xl font-bold mb-4">Datenschutzerklärung</h1>
+    <p>Hier steht deine Datenschutzerklärung...</p>
+  </div>
+);
+
+export default datenschutz;
