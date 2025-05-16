43be4900dc9e1e113c32341dc3104839d2fc33a1
eule-club <eulepro7@gmail.com>
2025-05-16 13:56:48 +0200

    Create agb.jsx

---
+++ eulepro/eulepro/src/pages/agb.jsx
@@ -0,0 +1,8 @@
+const agb = () => (
+  <div className="p-10">
+    <h1 className="text-2xl font-bold mb-4">Allgemeine Geschäftsbedingungen (AGB)</h1>
+    <p>Hier stehen deine AGB...</p>
+  </div>
+);
+
+export default agb;
