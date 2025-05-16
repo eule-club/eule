34e3555097300d9e2df06870cdffd93714e72c66
eule-club <eulepro7@gmail.com>
2025-05-16 13:59:18 +0200

    Update App.jsx

--- eulepro/eulepro/src/App.jsx
+++ eulepro/eulepro/src/App.jsx
@@ -1,5 +1,20 @@
-import React from 'react'; import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Impressum from "./pages/impressum";
+import AGB from "./pages/agb";
+import Datenschutz from "./pages/datenschutz";
 
-function App() { return <Home />; }
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<home />} />
+        <Route path="/impressum" element={<impressum />} />
+        <Route path="/agb" element={<agb />} />
+        <Route path="/datenschutz" element={<datenschutz />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
