7c095ee8d9fa3f18df080ca8a9b94f47c99665b6
eule-club <eulepro7@gmail.com>
2025-05-16 14:02:43 +0200

    Update Footer.jsx

--- eulepro/eulepro/src/components/Footer.jsx
+++ eulepro/eulepro/src/components/Footer.jsx
@@ -1,3 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Footer() { return (<footer className='p-4 bg-black text-white text-center'>© 2025 EULE PRO</footer>); }
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white py-6 px-8 flex flex-col md:flex-row justify-between items-center text-sm">
+      <div className="mb-4 md:mb-0">
+        © 2025 EULE. All rights reserved.
+      </div>
+      <div className="space-x-4">
+        <Link to="/impressum" className="hover:underline">
+          Impressum
+        </Link>
+        <Link to="/agb" className="hover:underline">
+          AGB
+        </Link>
+        <Link to="/datenschutz" className="hover:underline">
+          Datenschutz
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
