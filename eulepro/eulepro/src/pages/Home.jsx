84e9d03244eedd0d5666e2da6e703180ea265a60
eule-club <eulepro7@gmail.com>
2025-05-16 14:00:09 +0200

    Update and rename Home.jsx to home.jsx

--- eulepro/eulepro/src/pages/Home.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'; import Header from '../components/Header'; import Hero from '../components/Hero'; import FeatureSection from '../components/FeatureSection'; import ContactForm from '../components/ContactForm'; import Footer from '../components/Footer';
-
-export default function Home() { return (<div><Header /><Hero /><FeatureSection /><ContactForm /><Footer /></div>); }
---
+++ eulepro/eulepro/src/pages/home.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import FeatureSection from "../components/FeatureSection";
+import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <FeatureSection />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
